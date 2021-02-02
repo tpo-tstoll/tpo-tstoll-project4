9fd111eccfa6bae9308c0f8ca0e25a6da3b6371a
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-02-02 16:47:08 -0600

    2 functions created
    randomPhrase and createPhrases

--- js/Game.js
+++ js/Game.js
@@ -5,14 +5,15 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
     }
     startGame () {
 
     }
     getRandomPhrase () {
-
+        let randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return randomPhrase;
     }
     handleInteraction () {
 
@@ -24,6 +25,16 @@ class Game {
 
     }
     gameOver () {
-        
+
+    }
+    createPhrases () {
+        const phrases = [
+            new Phrase('shaken not stirred'),
+            new Phrase('the dude abides'),
+            new Phrase('you had me at hello'),
+            new Phrase('to infinity and beyond'),
+            new Phrase('houston we have a problem')
+        ];
+        return phrases;
     }
  }
